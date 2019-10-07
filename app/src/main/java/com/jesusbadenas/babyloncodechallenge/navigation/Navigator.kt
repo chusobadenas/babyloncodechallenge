ac973c0a9834f618014f61790a8f302a623eeadd
Jesús Badenas <chusobadenas@gmail.com>
2019-10-07 15:39:28 +0200

    Navigator class

---
+++ app/src/main/java/com/jesusbadenas/babyloncodechallenge/navigation/Navigator.kt
@@ -0,0 +1,26 @@
+package com.jesusbadenas.babyloncodechallenge.navigation
+
+import android.content.Context
+import com.jesusbadenas.babyloncodechallenge.postdetails.PostDetailsActivity
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class Navigator
+@Inject
+constructor() {
+
+    fun navigateToPostDetails(
+        context: Context?,
+        id: Int,
+        title: String,
+        body: String,
+        userId: Int
+    ) {
+        if (context != null) {
+            val intentToLaunch =
+                PostDetailsActivity.getCallingIntent(context, id, title, body, userId)
+            context.startActivity(intentToLaunch)
+        }
+    }
+}
