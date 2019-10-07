d3406091cc62e2f753ccf2ecb6075083b6fb7eb6
Jesús Badenas <chusobadenas@gmail.com>
2019-10-07 15:39:00 +0200

    Add Application class

---
+++ app/src/main/java/com/jesusbadenas/babyloncodechallenge/App.kt
@@ -0,0 +1,18 @@
+package com.jesusbadenas.babyloncodechallenge
+
+import com.jesusbadenas.babyloncodechallenge.di.components.DaggerApplicationComponent
+import dagger.android.AndroidInjector
+import dagger.android.DaggerApplication
+
+/**
+ * Android main Application
+ */
+class App : DaggerApplication() {
+
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerApplicationComponent
+            .builder()
+            .application(this)
+            .build()
+    }
+}
